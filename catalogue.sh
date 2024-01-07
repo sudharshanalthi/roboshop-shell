bea9023b884ae10be9e1aa0cabd3daa793c0437a
althi@sudharshan <althi@sudharshan.com>
2024-01-07 13:18:19 +0530

    roboshop shell scripts

--- catalogue.sh
+++ catalogue.sh
@@ -8,7 +8,7 @@ N="\e[0m"
 TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
-echo "script started executing at $TIMESTAMP" &>> $LOGFILE
+ echo "script started executing at $TIMESTAMP" &>> $LOGFILE
 
 VALIDATE(){
    if [ $1 -ne 0 ]
