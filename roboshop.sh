b6bd39a4a39f5f6dbed5d007c40f6a304e7e716a
althi@sudharshan <althi@sudharshan.com>
2024-01-18 00:07:39 +0530

    daws-76

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,7 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 
 for i in "${INSTANCES[@]}"
 do
-    echo "instance is: $i"
+    echo "instance name is: $i"
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         INSTANCE_TYPE="t3.small"
