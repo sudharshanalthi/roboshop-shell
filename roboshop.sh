9a73fe91afd5da4ccc541f001439a0fe311c3ee1
althi@sudharshan <althi@sudharshan.com>
2024-01-17 22:27:57 +0530

    daws-76

--- roboshop.sh
+++ roboshop.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 AMI=ami-03265a0778a880afb
-SG_ID=sg-00c327683078c0c60 
-INSTANCES=("mongodb" "redis" "mysql" "rabbit" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+SG_ID=sg-00c327683078c0c60 #replace with your SG ID
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 
 for i in "${INSTANCES[@]}"
 do
@@ -14,7 +14,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE t2.micro  --security-group-ids sg-00c327683078c0c60
+    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-00c327683078c0c60
 
     done
     
