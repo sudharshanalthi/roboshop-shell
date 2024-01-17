9cf7eb9f09915abd0a75e4aa4e0fb36506ff1c05
althi@sudharshan <althi@sudharshan.com>
2024-01-17 23:29:32 +0530

    daws-76

--- roboshop.sh
+++ roboshop.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 AMI=ami-03265a0778a880afb
-SG_ID=sg-00c327683078c0c60 #replace with your SG ID
+SG_ID=sg-00c327683078c0c60 
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 
 for i in "${INSTANCES[@]}"
 do
-    echo "instance is: $i"
-   if [ $i== "mongodb" ] || [ $i== "mysql" ] || [ $i == "shipping" ]
+    echo "instance is: $1"
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         INSTANCE_TYPE="t3.small"
     else
         INSTANCE_TYPE="t2.micro"
     fi
 
-    aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-00c327683078c0c60
-
-    done
-    
+   aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-00c327683078c0c60
+done
