5f3d8b2dc3a9a0cf8ae78bdfea948aebba150842
althi@sudharshan <althi@sudharshan.com>
2024-01-17 12:51:05 +0530

    daws-76

---
+++ roboshop.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+AMI=ami-03265a0778a880afb
+SG_ID=sg-00c327683078c0c60 
+INSTANCES=("mongodb" "redis" "mysql" "rabbit" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+
+for i in "${INSTANCES[@]}"
+do
+    echo "instance is: $i"
+   if [ $i== "mongodb" ] || [ $i== "mysql" ] || [ $i == "shipping" ]
+    then
+        INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t2.micro"
+    fi
+
+    aws ec2 run-instances --image-id ami-03265a0778a880afb --count 1 --instance-type t2.micro  --security-group-ids sg-00c327683078c0c60
+
+    done
